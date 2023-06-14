454cac2a083f4b01cc884ab9401745c0201c4ac4
abi <abhilashwithh@gmail.com>
2023-06-14 12:17:18 +0530

    intial commit

---
+++ src/test/java/com/projecto/baseapi/BaseapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecto.baseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaseapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
