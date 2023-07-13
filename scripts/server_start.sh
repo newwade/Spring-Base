5bfb669f848262bba35492e10eddbc13c87a1503
abi <abhilashwithh@gmail.com>
2023-07-13 11:48:15 +0530

    minor change

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -8,5 +8,5 @@ host=$(aws ssm get-parameters --region us-east-1 --names PSQLHOST --with-decrypt
 host=`echo $host | sed -e 's/^"//' -e 's/"$//'`
 port=$(aws ssm get-parameters --region us-east-1 --names PSQLPORT --with-decryption --query Parameters[0].Value)
 port=`echo $port | sed -e 's/^"//' -e 's/"$//'`
-sudo java -jar -DSQL_DB_HOST=$secret -DSQL_DB_USERNAME=$user -DSQL_DB_HOST=$host -DSQL_DB_PORT=$port \
+sudo java -jar -DSQL_DB_PASSWORD=$secret -DSQL_DB_USERNAME=$user -DSQL_DB_HOST=$host -DSQL_DB_PORT=$port \
     *.jar > /dev/null 2> /dev/null < /dev/null &
