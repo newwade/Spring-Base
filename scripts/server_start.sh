7a06b68837151921659838fc5056e795f78b9945
newwade <abhilashwithh@gmail.com>
2023-07-13 08:51:30 +0530

    changes

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
-sudo java -jar -DSQL_DB_PORT="5432" -DSQL_DB_USERNAME="postgres" -DSQL_DB_PASSWORD="rootroot" -DSQL_DB_HOST="jdbc:postgresql://postgres-1.cem8pqnfuddw.us-east-1.rds.amazonaws.com" \
+sudo java -jar  \
     *.jar > /dev/null 2> /dev/null < /dev/null &
